b70e3bd9856e58ba65c6dc5fbe23037dc9420d02
Olli Varila <olli.varila@gmail.com>
2023-09-11 18:24:42 +0300

    Fix postbuild

--- week2-3/postbuild.sh
+++ week2-3/postbuild.sh
@@ -8,3 +8,6 @@ cp prod-package.json dist/package.json
 # Copy Sharp (Cannot be bundled)
 mkdir -p dist/node_modules
 cp -r node_modules/sharp dist/node_modules/sharp
+
+# Install production dependencies
+npm install --prefix ./dist --omit=dev
