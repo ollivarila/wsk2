231ea052c259f14a8398a57f7e3718240a232bc4
Olli Varila <olli.varila@gmail.com>
2023-09-11 18:09:46 +0300

    Optimize build process

---
+++ week2-3/postbuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e # Abort on error
+
+# Copy production package.json
+cp prod-package.json dist/package.json
+
+# Copy Sharp (Cannot be bundled)
+mkdir -p dist/node_modules
+cp -r node_modules/sharp dist/node_modules/sharp
